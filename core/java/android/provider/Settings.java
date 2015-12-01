7d0179c038be3415cc85584db670d03c5f20b01f
rascarlo <rascarlo@gmail.com>
2015-12-01 19:18:53 +0100

    Volume Rocker Wake [1/2]
    Change-Id: Ia32e77b4737efd5816da1b4cfb74138fc357a46b
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4149,6 +4149,12 @@ public final class Settings {
         public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
 
         /**
+         * Volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
