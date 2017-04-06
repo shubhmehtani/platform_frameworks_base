15bb5287a71c9304d13676585367f77c9eaa2eb9
ezio84 <brabus84@gmail.com>
2017-04-06 19:36:47 +0200

    Ticker: allow to show music track info on new track being played [1/2]
    Change-Id: Ic14e06747ad2a5966ecf510cacd683476a80657d
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -423,7 +423,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     int mTrackingPosition; // the position of the top of the tracking view.
 
     // status bar notification ticker
-    private boolean mTickerEnabled;
+    private int mTickerEnabled;
     private Ticker mTicker;
     private View mTickerView;
     private boolean mTicking;
@@ -578,7 +578,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 		 mTickerEnabled = Settings.System.getIntForUser(
 		    mContext.getContentResolver(),
 		    Settings.System.STATUS_BAR_SHOW_TICKER,
-		    0, UserHandle.USER_CURRENT) == 1;
+		    0, UserHandle.USER_CURRENT);
                initTickerView();
 	    }
             updateSettings();
@@ -716,8 +716,29 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             if (DEBUG_MEDIA) Log.v(TAG, "DEBUG_MEDIA: onMetadataChanged: " + metadata);
             mMediaMetadata = metadata;
             updateMediaMetaData(true, true);
+            if (mTickerEnabled == 2) {
+                tickTrackInfo();
+            }
         }
     };
+
+    private void tickTrackInfo() {
+        ArrayList<Entry> activeNotifications = mNotificationData.getActiveNotifications();
+        int N = activeNotifications.size();
+        if (PlaybackState.STATE_PLAYING ==
+                getMediaControllerPlaybackState(mMediaController)
+                || PlaybackState.STATE_BUFFERING ==
+                getMediaControllerPlaybackState(mMediaController)) {
+            final String pkg = mMediaController.getPackageName();
+            for (int i = 0; i < N; i++) {
+                final Entry entry = activeNotifications.get(i);
+                if (entry.notification.getPackageName().equals(pkg)) {
+                    tick(entry.notification, true, true, mMediaMetadata);
+                    break;
+                }
+            }
+        }
+    }
 
     private final OnChildLocationsChangedListener mOnChildLocationsChangedListener =
             new OnChildLocationsChangedListener() {
@@ -1061,8 +1082,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mTickerEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.STATUS_BAR_SHOW_TICKER,
-                mContext.getResources().getBoolean(R.bool.enable_ticker)
-                        ? 1 : 1, UserHandle.USER_CURRENT) == 1;
+                0, UserHandle.USER_CURRENT);
         initTickerView();
 
         // set the initial view visibility
@@ -1527,7 +1547,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     private void initTickerView() {
-        if (mTickerEnabled && (mTicker == null || mTickerView == null)) {
+        if (mTickerEnabled != 0 && (mTicker == null || mTickerView == null)) {
             final ViewStub tickerStub = (ViewStub) mStatusBarView.findViewById(R.id.ticker_stub);
             if (tickerStub != null) {
                 mTickerView = tickerStub.inflate();
@@ -1536,7 +1556,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 TickerView tickerView = (TickerView) mStatusBarView.findViewById(R.id.tickerText);
                 tickerView.mTicker = mTicker;
             } else {
-                mTickerEnabled = false;
+                mTickerEnabled = 0;
             }
         }
     }
@@ -1753,7 +1773,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
             }
         } else {
-            tick(notification, true);
+            tick(notification, true, false, null);
         }
         addNotificationViews(shadeEntry, ranking);
         // Recalculate the position of the sliding windows and the titles.
@@ -1855,7 +1875,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         if (old != null) {
             // Cancel the ticker if it's still running
-            if (mTickerEnabled) {
+            if (mTickerEnabled != 0) {
                 mTicker.removeEntry(old);
             }
             if (CLOSE_PANEL_WHEN_EMPTIED && !hasActiveNotifications()
@@ -3534,8 +3554,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     @Override
-    protected void tick(StatusBarNotification n, boolean firstTime) {
-        if (!mTickerEnabled) return;
+    protected void tick(StatusBarNotification n, boolean firstTime, boolean isMusic, MediaMetadata metaMediaData) {
+        if (mTickerEnabled == 0) return;
 
         // no ticking in lights-out mode
         if (!areLightsOn()) return;
@@ -3550,11 +3570,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // until status bar window is attached to the window manager,
         // because...  well, what's the point otherwise?  And trying to
         // run a ticker without being attached will crash!
-        if (n.getNotification().tickerText != null && mStatusBarWindow != null
+        if ((!isMusic ? (n.getNotification().tickerText != null) : (metaMediaData != null)) && mStatusBarWindow != null
                 && mStatusBarWindow.getWindowToken() != null) {
             if (0 == (mDisabled1 & (StatusBarManager.DISABLE_NOTIFICATION_ICONS
                     | StatusBarManager.DISABLE_NOTIFICATION_TICKER))) {
-                mTicker.addEntry(n);
+                mTicker.addEntry(n, isMusic, metaMediaData);
             }
         }
     }
@@ -3562,14 +3582,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private class MyTicker extends Ticker {
         MyTicker(Context context, View sb) {
             super(context, sb);
-            if (!mTickerEnabled) {
-                Log.w(TAG, "MyTicker instantiated with mTickerEnabled=false", new Throwable());
+            if (mTickerEnabled == 0) {
+                Log.w(TAG, "MyTicker instantiated with mTickerEnabled=0", new Throwable());
             }
         }
 
         @Override
         public void tickerStarting() {
-            if (!mTickerEnabled) return;
+            if (mTickerEnabled == 0) return;
             mTicking = true;
             mStatusBarContents.setVisibility(View.GONE);
             mStatusBarContents.startAnimation(loadAnim(com.android.internal.R.anim.push_up_out,
@@ -3582,7 +3602,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         @Override
         public void tickerDone() {
-            if (!mTickerEnabled) return;
+            if (mTickerEnabled == 0) return;
             mStatusBarContents.setVisibility(View.VISIBLE);
             mStatusBarContents.startAnimation(loadAnim(com.android.internal.R.anim.push_down_in,
                     null));
@@ -3594,7 +3614,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         public void tickerHalting() {
-            if (!mTickerEnabled) return;
+            if (mTickerEnabled == 0) return;
             if (mStatusBarContents.getVisibility() != View.VISIBLE) {
                 mStatusBarContents.setVisibility(View.VISIBLE);
                 mStatusBarContents
@@ -3637,7 +3657,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             pw.println("  mExpandedVisible=" + mExpandedVisible
                     + ", mTrackingPosition=" + mTrackingPosition);
             pw.println("  mTickerEnabled=" + mTickerEnabled);
-            if (mTickerEnabled) {
+            if (mTickerEnabled != 0) {
                 pw.println("  mTicking=" + mTicking);
                 pw.println("  mTickerView: " + viewInfo(mTickerView));
             }
@@ -4226,7 +4246,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     @Override
     protected void haltTicker() {
-        if (mTickerEnabled) {
+        if (mTickerEnabled != 0) {
             mTicker.halt();
         }
     }
